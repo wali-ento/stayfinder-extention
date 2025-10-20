0f56ca4932abf95eea159fcba3978c82ce76b38a
shawn <shawn@teamento.com>
2025-10-20 15:42:07 +0500

    Update background message logging to remove emoji for a cleaner output.

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -3,7 +3,6 @@ export default defineBackground(() => {
   
   // Listen for messages from content script
   browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('📩 Received message in background:', message);
-    return true;
+    console.log('Received message in background:', message);
   });
 });
